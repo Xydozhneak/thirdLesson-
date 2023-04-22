2bdbc5e85cff6a0f6a345daa74dec16369e08643
unknown <xydozhnikbomj@gmail.com>
2023-04-22 21:48:43 +0300

    v1.0

---
+++ main.js
@@ -0,0 +1,16 @@
+firstValue=prompt('Enter 1st number' );
+lastValue=prompt('Enter 2nd number' );
+
+difference=firstValue-lastValue;
+summ= parseFloat(firstValue)+parseFloat(lastValue);
+multipl=firstValue*lastValue;
+divide=firstValue/lastValue;
+alert(` ${firstValue} + ${lastValue} = ${summ} 
+
+${firstValue}-${lastValue}= ${difference}
+
+${firstValue}*${lastValue}=${multipl}
+
+${firstValue}/${lastValue}=${divide}
+
+`);
